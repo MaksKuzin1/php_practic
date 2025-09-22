def6ca181b3ce3b137261d7c53e7e08dd19b4525
Maks Kuzin <maks.kuzin5544@gmail.com>
2025-09-22 11:39:07 +0500

    tt

--- array2/index.php
+++ array2/index.php
@@ -9,7 +9,33 @@
     задача на одномерный массив 3
     <br>
     <?php
-
+    $N = 10;
+    $array = [];
+    $sum = 0;
+    $count = 0;
+    for ($i=0; $i < $N; $i++) { 
+        $array[] = mt_rand(1, 10);
+        echo $array[$i] . ', ';
+    } 
+    echo"<hr>";
+    foreach ($array as $value) {
+        $value = ($value < 2) ? 0 : $value;
+        echo $value . ', ';
+        if ($value > 2 && $value < 7) {
+            $sum += $value;
+            $count += 1;
+        }
+    }
+    echo"<hr>";
+    echo"Сумма эл. от 3 до 6 = $sum <br> Кол-во = $count"
+    ?>
+    <hr>
+    <br>
+    задача на одномерный массив 21
+    <br>
+    <?php
+    $N = 10;
+    
     ?>
     <hr>
 </body>
